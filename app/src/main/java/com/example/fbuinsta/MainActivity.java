75d3530793553babbf03ebcff833a85633dd24e5
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-08 15:27:33 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fbuinsta/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.fbuinsta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+    }
+}
